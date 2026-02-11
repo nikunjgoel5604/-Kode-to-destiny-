b314a108b7329b778f6094c440b2ed7f3ac6f629
Nikunj Goel <kunjgoel2004@gmail.com>
2026-02-11 11:06:50 +0530

    update

--- DMBS- SQL/SQL Commands.sql
+++ DMBS- SQL/SQL Commands.sql
@@ -13,3 +13,22 @@ for That SQL commands are divide in to 5 categories :-
 create database college_db; -- we create the Databse with the name college_db;
 show databases;  -- now we have create the databse so check it is the database create sucessfull or not so we use show database to know about that.
 use college_db;  -- now we know that our database is create sucessfull . so use acesses that database we use a command use college_db (here college_db is the database name).
+create table student (
+student_id int primary key, -- here you see we make a key primary key . we study about that quite later.
+name varchar(255),
+age int);
+
+alter table student add city varchar(255); -- alter is use to add or remove something from the table 
+truncate table student; --  Remove all records but keep structure
+-- drop table student; --  # Delete table permanently with all the record and drop also use to delete the database complete.
+
+
+-- 
+
+
+
+
+
+
+
+
