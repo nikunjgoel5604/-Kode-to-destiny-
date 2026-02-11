5b914defe3011d1c51c1007e51553c1263bdf940
Nikunj Goel <kunjgoel2004@gmail.com>
2026-02-11 10:49:28 +0530

    update

--- DMBS- SQL/SQL Commands.sql
+++ DMBS- SQL/SQL Commands.sql
@@ -5,42 +5,8 @@ for That SQL commands are divide in to 5 categories :-
                                               3. DQL - Data Query Language 
                                               4. DCL - Data Control Langauge 
                                               5. TCL - Transaction Control Langauge */
-
-CREATE DATABASE CollegeDB;
-USE CollegeDB;
-
-CREATE TABLE StudentCourse (
-    StudentID VARCHAR(5),
-    StudentName VARCHAR(50),
-    CourseID VARCHAR(5),
-    CourseName VARCHAR(50),
-    Instructor VARCHAR(50),
-    InstructorPhone VARCHAR(15)
-);
-INSERT INTO StudentCourse VALUES
-('S1', 'Alice', 'C1', 'DBMS', 'Dr. Rao', '999111'),
-('S1', 'Alice', 'C2', 'OS', 'Dr. Mehta', '999222'),
-('S2', 'Bob',   'C1', 'DBMS', 'Dr. Rao', '999111'),
-('S3', 'Carol', 'C3', 'CN',   'Dr. Singh', '999333');
-
-SET SQL_SAFE_UPDATES = 0;
-
-select * from StudentCourse;
-UPDATE StudentCourse
-SET InstructorPhone = '888111'
-WHERE Instructor = 'Dr. Rao';
-select * from StudentCourse;
-
-UPDATE StudentCourse
-SET StudentID = NULL
-WHERE Instructor = 'Dr. Rao';
-select * from StudentCourse;
-
-UPDATE StudentCourse
-SET StudentID = NULL,
-    StudentName = NULL,
-    CourseID = NULL
-WHERE Instructor = 'Dr. Rao';
-select * from StudentCourse;
-
+                                              
+-- 1. DDL command are use to create and modify databse struture
+-- we use DDL becuse before inser tdata we must define structure 
+-- DDL 	command are :-  CREATE , ALTER , DROP TRUNCATE , RENAME.
 
